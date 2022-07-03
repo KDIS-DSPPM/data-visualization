47117042d37bc882f5bb35a36f3273d1ba66da77
Jae Yeon Kim <jaeyeonkim@kdis.ac.kr>
2022-07-03 17:12:59 +0900

    docker setup

---
+++ install.r
@@ -0,0 +1,11 @@
+install.packages(c("pacman",
+                   "tidyverse",
+                   "broom",
+                   "devtools",
+                   "here", # computational reproducibility
+                   "gapminder", # toy data
+                   "ggthemes", # additional themes
+                   "ggrepel", # arranging ggplots
+                   "patchwork", # arranging ggplots
+                   "gtsummary"
+              	   ))
